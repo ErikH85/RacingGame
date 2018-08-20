086aebc58530879e399130585c0abe09e589b867
Erik <erikhansson85@gmail.com>
2018-08-20 11:58:26 +0200

    test

---
+++ src/main/java/com/example/RacingGame/RacingGameApplication.java
@@ -0,0 +1,12 @@
+package com.example.RacingGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingGameApplication.class, args);
+	}
+}
