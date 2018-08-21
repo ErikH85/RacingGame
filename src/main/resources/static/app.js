eae3f46b56f1c98707518dd5f310af6cc1a50a28
Christoffer <christoffergunnarsson@hotmail.com>
2018-08-21 08:56:41 +0200

    made the car and background to move

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -16,26 +16,101 @@ PIXI.loader
     .add("Audi.png")
   .load(setup);
 
+let audi, state,road;
 
 function setup() {
 
-  let road = new PIXI.Sprite(PIXI.loader.resources["road1.png"].texture);
-  let audi = new PIXI.Sprite(PIXI.loader.resources["Audi.png"].texture);
+  road = new PIXI.Sprite(PIXI.loader.resources["road1.png"].texture);
+  audi = new PIXI.Sprite(PIXI.loader.resources["Audi.png"].texture);
 
   audi.x = 350;
   audi.y = 250;
-
-
-    app.ticker.add(function(delta) {
-        // just for fun, let's rotate mr rabbit a little
-        // delta is 1 if running at 100% performance
-        // creates frame-independent transformation
-        audi.y -= 0.5 * delta;
-    });
-
-
+  audi.vx =0;
+  audi.vy=0;
+    road.x = 0;
+    road.y = 0;
+    road.vx =0;
+    road.vy=0;
   app.stage.addChild(road);
   app.stage.addChild(audi);
+
+    let left = keyboard(37),
+        up = keyboard(38),
+        right = keyboard(39),
+        down = keyboard(40);
+
+    //Left arrow key `press` method
+    left.press = () => {
+        //Change the cat's velocity when the key is pressed
+        audi.vx = -5;
+        audi.vy = 0;
+    };
+
+    //Left arrow key `release` method
+    left.release = () => {
+        //If the left arrow has been released, and the right arrow isn't down,
+        //and the cat isn't moving vertically:
+        //Stop the cat
+        if (!right.isDown && audi.vy === 0) {
+            audi.vx = 0;
+        }
+    };
+
+    //Up
+    up.press = () => {
+        road.vy = -5;
+        road.vx = 0;
+    };
+    up.release = () => {
+        if (!down.isDown && road.vx === 0) {
+            road.vy = 0;
+        }
+    };
+
+    //Right
+    right.press = () => {
+        audi.vx = 5;
+        audi.vy = 0;
+    };
+    right.release = () => {
+        if (!left.isDown && audi.vy === 0) {
+            audi.vx = 0;
+        }
+    };
+
+    //Down
+    down.press = () => {
+        road.vy = 5;
+        road.vx = 0;
+    };
+    down.release = () => {
+        if (!up.isDown && road.vx === 0) {
+            road.vy = 0;
+        }
+    };
+
+    //Set the game state
+    state = play;
+
+    //Start the game loop
+    app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta){
+
+    //Update the current game state:
+    state(delta);
+}
+
+function play(delta) {
+
+    //Use the cat's velocity to make it move
+    audi.x += audi.vx;
+    audi.y += audi.vy;
+    road.x += road.vx;
+    road.y += road.vy;
+
+
 }
 
 function update(){
@@ -43,7 +118,42 @@ function update(){
 
 }
 
+function keyboard(keyCode) {
+    let key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
 
+    //The `upHandler`
+    key.upHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+
+    //Attach event listeners
+    window.addEventListener(
+        "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+        "keyup", key.upHandler.bind(key), false
+    );
+    return key;
+}
 
 
 
