3405122428c14a8f0e6c934377dc0a48bc3b619c
Christoffer <christoffergunnarsson@hotmail.com>
2018-08-28 16:12:52 +0200

    .

--- src/main/java/com/example/RacingGame/ScoreRepository.java
+++ src/main/java/com/example/RacingGame/ScoreRepository.java
@@ -1,4 +1,55 @@
 package com.example.RacingGame;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ScoreRepository {
+
+    @Autowired
+    public DataSource dataSource;
+
+    public void addHighscore(int id,int score) {
+
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO [statistics] VALUES(?,?, ?)");
+            ps.setInt(1,id);
+            ps.setInt(2,score);
+            ps.setNull(3,1);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public List <String> getHighscores(){
+
+        List <String> scores = new ArrayList<>();
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT TOP (3) dbo.users.username, dbo.[statistics].highscore FROM dbo.[statistics] INNER JOIN dbo.users ON dbo.[statistics].userID = dbo.users.userID ORDER BY dbo.[statistics].highscore DESC");
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while(resultSet.next()){
+                scores.add(resultSet.getString("username"));
+                scores.add(resultSet.getString("highscore"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return scores;
+    }
 }
